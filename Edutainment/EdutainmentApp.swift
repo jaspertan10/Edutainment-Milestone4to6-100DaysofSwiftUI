5a0f49cd142646af5ab9d7f8ea8cd06ae6cf0ba1
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2024-11-18 21:57:46 -0800

    initial commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Jasper Tan on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
